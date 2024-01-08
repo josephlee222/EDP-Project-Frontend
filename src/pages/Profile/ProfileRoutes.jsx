9732ed300b6797bc89c006d13402d19962cd11a2
Joseph Lee <facebooklee52@gmail.com>
2024-01-08 21:17:27 +0800

    implement gravatar profile picture

--- src/pages/Profile/ProfileRoutes.jsx
+++ src/pages/Profile/ProfileRoutes.jsx
@@ -4,15 +4,20 @@ import NotFound from '../errors/NotFound'
 import Test from '../Test'
 import { AppContext } from '../../App'
 import { useSnackbar } from 'notistack'
-import { Card, CardContent, Container, Grid, ListItemIcon, ListItemButton, ListItem, ListItemText, createTheme, ThemeProvider, Box, Typography, Button } from '@mui/material'
+import { Card, CardContent, Container, Grid, Box, Typography, Button, Dialog, DialogTitle, DialogActions, DialogContent, IconButton, DialogContentText, Stack, Tooltip } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
 import ProfilePicture from '../../components/ProfilePicture'
 import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
 import TodayRoundedIcon from '@mui/icons-material/TodayRounded';
 import SecurityRoundedIcon from '@mui/icons-material/SecurityRounded';
 import AccountBalanceWalletRoundedIcon from '@mui/icons-material/AccountBalanceWalletRounded';
+import CloseIcon from '@mui/icons-material/CloseRounded';
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import PublicIcon from '@mui/icons-material/PublicRounded';
+import FileUploadIcon from '@mui/icons-material/FileUploadRounded';
 import PageHeader from '../../components/PageHeader'
 import { validateUser } from "../../functions/user";
+import http from "../../http";
 
 import ViewProfile from './ViewProfile'
 import ViewBookings from './ViewBookings'
@@ -22,10 +27,66 @@ import ViewSecurity from './ViewSecurity'
 export const ProfileContext = createContext(null);
 
 export default function ProfileRoutes() {
-    const { setAdminPage, user } = useContext(AppContext);
+    const { setAdminPage, user, setUser } = useContext(AppContext);
     const { enqueueSnackbar } = useSnackbar();
-    const [ activePage, setActivePage ] = useState(null);
+    const [activePage, setActivePage] = useState(null);
     const navigate = useNavigate();
+    const [changePictureDialog, setChangePictureDialog] = useState(false);
+    const [loadingPicture, setLoadingPicture] = useState(false);
+
+    const handleChangePictureDialogClose = () => {
+        setChangePictureDialog(false);
+    }
+
+    const handleChangePictureDialogOpen = () => {
+        setChangePictureDialog(true);
+    }
+
+    const handlePictureChange = (e) => {
+        setLoadingPicture(true);
+        console.log(e);
+        const formData = new FormData();
+        formData.append("profile_picture", e.target.files[0]);
+        http.post("/user/upload", formData, { headers: { "Content-Type": "multipart/form-data" } }).then((res) => {
+            if (res.status === 200) {
+                enqueueSnackbar("Profile picture updated successfully!", { variant: "success" });
+                setUser(res.data);
+                setLoadingPicture(false);
+                handleChangePictureDialogClose();
+            } else {
+                enqueueSnackbar("Profile picture update failed!", { variant: "error" });
+                setLoadingPicture(false);
+                handleChangePictureDialogClose();
+            }
+        }).catch((err) => {
+            enqueueSnackbar("Profile picture update failed! " + err.response.data.message, { variant: "error" });
+            setLoadingPicture(false);
+            handleChangePictureDialogClose();
+        })
+    }
+
+    const handleGravatarChange = () => {
+        setLoadingPicture(true);
+        const data = {
+            profilePictureType: "gravatar"
+        }
+        http.put("/User", data).then((res) => {
+            if (res.status === 200) {
+                enqueueSnackbar("Profile picture updated successfully!", { variant: "success" });
+                setUser(res.data);
+                setLoadingPicture(false);
+                handleChangePictureDialogClose();
+            } else {
+                enqueueSnackbar("Profile picture update failed!", { variant: "error" });
+                setLoadingPicture(false);
+                handleChangePictureDialogClose();
+            }
+        }).catch((err) => {
+            enqueueSnackbar("Profile picture update failed! " + err.response.data.message, { variant: "error" });
+            setLoadingPicture(false);
+            handleChangePictureDialogClose();
+        })
+    }
 
     useEffect(() => {
         if (!validateUser()) {
@@ -44,11 +105,15 @@ export default function ProfileRoutes() {
                             <CardContent>
                                 <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
                                     {user &&
-                                        <ProfilePicture user={user} sx={{ width: ["72px", "96px", "128px"], height: ["72px", "96px", "128px"] }} />
+                                        <Tooltip title="Change Profile Picture" arrow>
+                                            <IconButton onClick={handleChangePictureDialogOpen}>
+                                                <ProfilePicture user={user} sx={{ width: ["72px", "96px", "128px"], height: ["72px", "96px", "128px"] }} />
+                                            </IconButton>
+                                        </Tooltip>
                                     }
                                     <Typography variant="h5" fontWeight={700} sx={{ textAlign: "center", mt: ".5rem" }}>{user && user.name}</Typography>
                                     <Typography variant="body1" sx={{ textAlign: "center" }}>{user && user.email}</Typography>
-                                    <Button fullWidth variant="contained" sx={{ mt: "1rem" }} LinkComponent={Link} to="/profile/edit" startIcon={<EditRoundedIcon/>}>Edit Profile</Button>
+                                    <Button fullWidth variant="contained" sx={{ mt: "1rem" }} LinkComponent={Link} to="/profile/edit" startIcon={<EditRoundedIcon />}>Edit Profile</Button>
                                 </Box>
                             </CardContent>
                         </Card>
@@ -56,7 +121,7 @@ export default function ProfileRoutes() {
                     <Grid item xs={12} md="8" lg="9">
                         <Card sx={{ mt: "1rem" }}>
                             <CardContent>
-                                <Box sx={{  alignItems: "center", overflowX:"auto", whiteSpace:"nowrap" }}>
+                                <Box sx={{ alignItems: "center", overflowX: "auto", whiteSpace: "nowrap" }}>
                                     <Button variant={activePage == 1 ? "contained" : "secondary"} startIcon={<BadgeRoundedIcon />} sx={{ mr: ".5rem", fontWeight: 700 }} LinkComponent={Link} to="/profile">Profile Information</Button>
                                     <Button variant={activePage == 2 ? "contained" : "secondary"} startIcon={<TodayRoundedIcon />} sx={{ mr: ".5rem", fontWeight: 700 }} LinkComponent={Link} to="/profile/bookings">Bookings</Button>
                                     <Button variant={activePage == 3 ? "contained" : "secondary"} startIcon={<AccountBalanceWalletRoundedIcon />} sx={{ mr: ".5rem", fontWeight: 700 }} LinkComponent={Link} to="/profile/wallet">Wallet & Gifts</Button>
@@ -74,6 +139,27 @@ export default function ProfileRoutes() {
                     </Grid>
                 </Grid>
             </Container>
+            <Dialog open={changePictureDialog} onClose={handleChangePictureDialogClose}>
+                <DialogTitle>Change Profile Picture</DialogTitle>
+                <Box component="form">
+                    <DialogContent sx={{ paddingTop: 0 }}>
+                        <DialogContentText>
+                            You are currently using a {user ? user.profilePictureType === "gravatar" ? "Gravatar" : "local" : "unknown"} profile picture.
+                            <br /><br />
+                            You can select a new profile picture from your computer or from Gravatar. To set a new profile picture from your computer, click on the "Upload Image" button below. To set a new profile picture from Gravatar, click on the "Use Gravatar" button below.
+                            <br /><br />
+                            For information on how to set a profile picture on Gravatar, please visit <a href="https://en.gravatar.com/support/">https://en.gravatar.com/support/</a>.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Stack direction={["column", "row"]} spacing={1}>
+                            <Button style={{ justifyContent: "flex-end" }} onClick={handleChangePictureDialogClose} startIcon={<CloseIcon />}>Cancel</Button>
+                            <LoadingButton style={{ justifyContent: "flex-end" }} loadingPosition="start" loading={loadingPicture} variant="text" color="primary" startIcon={<PublicIcon />} onClick={handleGravatarChange}>Use Gravatar</LoadingButton>
+                            <LoadingButton style={{ justifyContent: "flex-end" }} loadingPosition="start" loading={loadingPicture} variant="text" color="primary" startIcon={<FileUploadIcon />} component="label">Upload Image<input type='file' onChange={handlePictureChange} hidden /></LoadingButton>
+                        </Stack>
+                    </DialogActions>
+                </Box>
+            </Dialog>
         </ProfileContext.Provider>
     )
 }
