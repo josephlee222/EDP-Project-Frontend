e01a5a97b4781ad0135dc6b8639ae71d65fc9857
Joseph Lee <facebooklee52@gmail.com>
2024-01-13 00:45:01 +0800

    Newsletter subscription

--- src/pages/Profile/ViewProfile.jsx
+++ src/pages/Profile/ViewProfile.jsx
@@ -3,13 +3,34 @@ import { Box, Card, CardContent, Grid, Typography, Button } from "@mui/material"
 import { AppContext } from "../../App";
 import { ProfileContext } from "./ProfileRoutes";
 import CardTitle from "../../components/CardTitle";
-import { PersonRounded, NewspaperRounded } from "@mui/icons-material";
+import { PersonRounded, NewspaperRounded, CloseRounded } from "@mui/icons-material";
 import InfoBox from "../../components/InfoBox";
+import http from "../../http";
+import { useSnackbar } from "notistack";
+import { LoadingButton } from "@mui/lab";
 
 
 export default function ViewProfile() {
-    const { user } = useContext(AppContext);
-    const { activePage, setActivePage } = useContext(ProfileContext);
+    const { user, setUser } = useContext(AppContext);
+    const { setActivePage } = useContext(ProfileContext);
+    const [loading, setLoading] = useState(false);
+    const { enqueueSnackbar } = useSnackbar();
+
+    const handleNewsletterSubscription = () => {
+        setLoading(true);
+        http.put("/User", { newsletter: !user.newsletter })
+        .then((res) => {
+            setUser(res.data);
+            enqueueSnackbar("Newsletter subscription status updated.", { variant: "success" })
+        })
+        .catch((err) => {
+            console.log(err);
+            enqueueSnackbar("An error occurred while updating your newsletter subscription status.", { variant: "error" })
+        })
+        .finally(() => {
+            setLoading(false);
+        })
+    }
 
     useEffect(() => {
         setActivePage(1);
@@ -46,7 +67,7 @@ export default function ViewProfile() {
                             <InfoBox title="Membership Status" value={"Not implemented"} />
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <InfoBox title="Newsletter subscription" boolean={false} value="Not Implemented" />
+                            <InfoBox title="Newsletter subscription" boolean={user?.newsletter} value={user?.newsletter ? "Subscribed" : "Not Subscribed"} />
                         </Grid>
                         <Grid item xs={12} md={6}>
                             <InfoBox title="Address" value={user && (user.address ? user.address : "Not Provided")} />
@@ -61,7 +82,7 @@ export default function ViewProfile() {
                 <CardContent>
                     <CardTitle title="Newsletter Subscription" icon={<NewspaperRounded />} />
                     <Typography variant="body1" mt={3}>Subscribe to our monthly newsletter to keep up-to-date on the latest NTUC UPlay offerings and discounts! Subscription to the newsletter is optional and can be disable anytime on this menu.</Typography>
-                    <Button variant="contained" sx={{ mt: 3 }} startIcon={<NewspaperRounded />} fullWidth>Subscribe to Newsletter</Button>
+                    <LoadingButton variant="contained" sx={{ mt: 3 }} startIcon={user?.newsletter ? <CloseRounded/> : <NewspaperRounded />} fullWidth onClick={handleNewsletterSubscription} loading={loading} loadingPosition="start">{user?.newsletter ? "Unsubscribe to newsletter" : "Subscribe to Newsletter"}</LoadingButton>
                 </CardContent>
             </Card>
         </>
